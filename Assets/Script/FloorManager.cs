121b019723cd87caa5a7dd21abb056e186acf4df
ABEL K ANTONY <abelkantony98@gmail.com>
2023-07-26 19:23:34 +0530

    Player movement done

---
+++ Assets/Script/FloorManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorManager : MonoBehaviour
+{
+    [SerializeField] GameObject floorPrefab;
+    private int y = 15;
+
+    private void Awake()
+    {
+        for(int i =0; i<10;i++)
+        {
+            GameObject floor = Instantiate(floorPrefab);
+            int x = Random.Range(-30, 30);
+            floor.transform.position = new Vector3(x, y, 0);
+            y = y - 5;
+        }
+    }
+}
--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
     private float speed = 5f;
+    private float jumpForce = 1500f;
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -16,6 +17,15 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.D)||Input.GetKey(KeyCode.RightArrow))
         {
             rb.AddForce(Vector2.right*speed);
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.AddForce(Vector2.left * speed);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * jumpForce);
+            Debug.Log("Jump");
         }
     }
 }
