7908f590dcc911aafb6066ba4c53525d4a5b1ef1
ABEL K ANTONY <abelkantony98@gmail.com>
2023-07-29 20:17:43 +0530

    floor repositioning done with zero error

--- Assets/Script/FloorRepositioning.cs
+++ Assets/Script/FloorRepositioning.cs
@@ -10,13 +10,23 @@ public class FloorRepositioning : MonoBehaviour
          floorManager = FindAnyObjectByType<FloorManager>();
 
     }
-    private void OnCollisionEnter2D(Collision2D collision)
+ /*   private void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("Floorr collide");
-        if(collision.gameObject.tag=="Floor")
+        if (collision.gameObject.tag == "Floor")
         {
             floorManager.RepositionFloor(this.gameObject);
             Debug.Log("Floor RERppossitioning");
+        }
+    }*/
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("trigger");
+        if(collision.gameObject.tag =="Floor")
+        {
+            floorManager.RepositionFloor(collision.gameObject);
+            Debug.Log(collision);
+            Debug.Log("triggeer repposition");
         }
     }
 }
