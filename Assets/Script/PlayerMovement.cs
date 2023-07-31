e78dfbca6058e4ef09f2d91c6c11aeb764d0e240
ABEL K ANTONY <abelkantony98@gmail.com>
2023-07-31 22:41:52 +0530

    animation to player

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -4,28 +4,49 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private SpriteRenderer spriteRenderer;
     private Rigidbody2D rb;
     private float speed = 5f;
     private float jumpForce = 1500f;
+
+    public Sprite[] sprites;
+    private int spriteIndex;
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        AnimateSprite();
     }
+
+   
 
     private void Update()
     {
         if (Input.GetKey(KeyCode.D)||Input.GetKey(KeyCode.RightArrow))
         {
             rb.AddForce(Vector2.right*speed);
+            AnimateSprite();
         }
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
             rb.AddForce(Vector2.left * speed);
+            AnimateSprite();
         }
         if (Input.GetKeyDown(KeyCode.Space))
         {
             rb.AddForce(Vector2.up * jumpForce);
             Debug.Log("Jump");
         }
+    }
+
+    private void AnimateSprite()
+    {
+        spriteIndex++;
+        if(spriteIndex>=sprites.Length)
+        {
+            spriteIndex = 0;
+        }
+
+        spriteRenderer.sprite = sprites[spriteIndex];
     }
 }
