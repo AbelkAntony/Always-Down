925d78fac4bb21b445abc7b9c0fa31ba641dfd70
ABEL K ANTONY <abelkantony98@gmail.com>
2023-08-24 02:25:22 +0530

    new enemy adding

---
+++ Assets/Script/Enemies Scripts/Fan.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fan : MonoBehaviour
+{
+
+    private EnemyManager enemyManager;
+    private int damagePoint = 20;
+    private void Start()
+    {
+        enemyManager = FindAnyObjectByType<EnemyManager>();
+    }
+    private void Update()
+    {
+        this.gameObject.transform.Rotate(0f, 0.0f, .1f, Space.Self);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == "Player")
+        {
+            enemyManager.PlayerTakeDamage(damagePoint);
+        }
+    }
+}
