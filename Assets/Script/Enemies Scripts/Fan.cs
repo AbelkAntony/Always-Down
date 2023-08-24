4e15d32ebb958245a58e2b200a92414b90f9c03d
ABEL K ANTONY <abelkantony98@gmail.com>
2023-08-24 19:14:31 +0530

    animation for fan obstacle done

--- Assets/Script/Enemies Scripts/Fan.cs
+++ Assets/Script/Enemies Scripts/Fan.cs
@@ -7,6 +7,8 @@ public class Fan : MonoBehaviour
 
     private EnemyManager enemyManager;
     private int damagePoint = 20;
+    private int life = 5;
+    private int point = 15;
     private void Start()
     {
         enemyManager = FindAnyObjectByType<EnemyManager>();
@@ -14,6 +16,10 @@ public class Fan : MonoBehaviour
     private void Update()
     {
         this.gameObject.transform.Rotate(0f, 0.0f, .1f, Space.Self);
+       if(this.gameObject.name=="Circle")
+        {
+            this.gameObject.transform.Rotate(0f, 0.0f, 1f, Space.Self);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
