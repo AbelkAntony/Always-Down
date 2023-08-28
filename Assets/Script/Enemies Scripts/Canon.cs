83eddb0644bf85356e3b5dea5665ab0f367520b6
ABEL K ANTONY <abelkantony98@gmail.com>
2023-08-28 19:27:27 +0530

    update to canon

--- Assets/Script/Enemies Scripts/Canon.cs
+++ Assets/Script/Enemies Scripts/Canon.cs
@@ -6,18 +6,31 @@ public class Canon : MonoBehaviour
 {
     [SerializeField] GameObject gun;
     [SerializeField] EnemyManager enemyManager;
+    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] GameObject gunPoint;
+    private Vector3 targetDirection;
     private float speed = 10f;
+    private float bulletSpeed = 100f;
 
     private void Start()
     {
         enemyManager = FindAnyObjectByType<EnemyManager>();
+        InvokeRepeating(nameof(Fire), 2, 2);
+
     }
     private void Update()
     {
         float singleStep = speed * Time.deltaTime;
-        Vector3 targetDirection = enemyManager.GetPlayerPosition() -gun.transform.position;
+        targetDirection = enemyManager.GetPlayerPosition() -gun.transform.position;
         float rotZ = Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg;
-
         gun.transform.rotation = Quaternion.Euler(0,0,rotZ);
     }
+    
+    private void Fire()
+    {
+        Vector3 bulletDirection = enemyManager.GetPlayerPosition();
+        GameObject bullet = Instantiate(bulletPrefab, gunPoint.transform.position, gunPoint.transform.rotation);
+        bullet.GetComponent<Rigidbody2D>().AddForce(targetDirection*bulletSpeed);
+    }
 }
+
